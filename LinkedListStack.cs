279d91177111909740f9de1b761148ae6cc87092
Vinay <navik.vinay12@gmail.com>
2022-11-28 14:31:40 +0530

    creating custom stack

---
+++ LinkedListStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedListMethods
+{
+    public class LinkedListStack
+    {
+        public Node top;
+        public void Push(int data)      //UC1 .
+        {
+            Node newNode = new Node(data);
+            if(top!=null)
+            {
+                newNode.next=top;   //newNode will start holding the address which was earlier holding by top.
+            }
+            top = newNode;  //top will start pointing to newNode.
+            Console.WriteLine($"New Node {data} is added.");
+        }
+
+        public void Display()           //Display()
+        {
+            Console.WriteLine("Displaying Nodes ");
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine("Value in the stack : "+temp.data);
+                    temp = temp.next;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stack Program in Data Structures");
+            Console.WriteLine("Please select as per the below given options :\n" +
+                "Press 1: Creating custom stack using LinkedList.\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LinkedListStack stackObject=new LinkedListStack();
+                    stackObject.Push(70);
+                    stackObject.Push(30);
+                    stackObject.Push(56);
+                    stackObject.Display();
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
