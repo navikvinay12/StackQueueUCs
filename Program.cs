5236b513e2a6d9c3c70bdce55295b397a226de36
Vinay <navik.vinay12@gmail.com>
2022-11-28 12:47:11 +0530

    welcome msg in Stack Queue program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StackUsingLinkedListMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack Program in Data Structures");
+        }
+    }
+}
